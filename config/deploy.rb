99a3a4b9a4f8aa039a730fc8483751f7cafa414a
Cristian Pereyra <cristian@redmintlabs.com>
2014-08-31 15:54:23 -0300

    fix issue around staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.2.1'
 set :application, 'tix_production'
 set :repo_url, 'git@github.com:joseignaciosg/TiX.git'
 set :format, :pretty
-set :log_level, :info
+set :log_level, :debug
 set :pty, true
 set :keep_releases, 5
 
