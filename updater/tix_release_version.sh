4d38aa938c0fe04eadf00cf6f6253866625c744d
Cristian Pereyra <cristian@redmintlabs.com>
2014-09-13 15:17:18 -0300

    improve release script

--- updater/tix_release_version.sh
+++ updater/tix_release_version.sh
@@ -6,6 +6,8 @@ source $DIR/tix_lib.sh
 get_os
 get_variant
 
+current_branch = $(git rev-parse --abbrev-ref HEAD)
+
 echo "Moving to 'releases' branch"
 git checkout origin/releases;
 git checkout releases;
@@ -14,7 +16,14 @@ git checkout -b release/$os/$variant
 
 echo "Clearing 'releases' folder"
 rm -rf releases/*
-echo "Copying files from master"
-git checkout master -- releases/
+echo "Copying files from $current_branch"
+git checkout $current_branch -- releases/
+echo "Commiting release"
+git commit -m "Release sources for $os/$variant/head"
 echo "Creating tag"
 git tag $os/$variant/head --force
+echo "Created tag $os/$variant/head on branch release/$os/$variant"
+echo "Finishing..."
+git checkout $current_branch
+echo "DONE!"
+
