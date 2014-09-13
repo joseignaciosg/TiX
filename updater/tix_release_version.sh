a1c7a91bc60fb7858f6e397b1fcba0e1e43f8904
Cristian Pereyra <cristian@redmintlabs.com>
2014-09-13 15:06:18 -0300

    tix release version script

--- updater/tix_release_version.sh
+++ updater/tix_release_version.sh
@@ -6,4 +6,15 @@ source $DIR/tix_lib.sh
 get_os
 get_variant
 
+echo "Moving to 'releases' branch"
+echo git checkout origin/releases;
+echo git checkout releases;
+echo "Creating or moving to branch release/$os/$variant"
+echo git checkout -b release/$os/$variant
 
+echo "Clearing 'releases' folder"
+echo rm -rf releases/*
+echo "Copying files from master"
+echo git checkout master -- releases/
+echo "Creating tag"
+echo git tag $os/$variant/head --force
