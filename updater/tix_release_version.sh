f04ccbdde4f939252af99bbe8f383adb985b6560
Cristian Pereyra <cristian@redmintlabs.com>
2014-09-13 15:57:42 -0300

    extract into function

--- updater/tix_release_version.sh
+++ updater/tix_release_version.sh
@@ -8,23 +8,29 @@ get_variant
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-echo "Moving to 'releases' branch"
-git checkout origin/releases;
-git checkout releases;
-echo "Creating or moving to branch release/$os/$variant"
-git checkout -b release/$os/$variant
+create_new_tag() {
+  echo "Moving to 'releases' branch"
+  git checkout origin/releases;
+  git checkout releases;
+  echo "Creating or moving to branch release/$os/$variant"
+  git checkout -b release/$os/$variant
+  echo "Clearing 'releases' folder"
+  rm -rf releases/*
+  echo "Copying files from $current_branch"
+  git checkout $current_branch -- releases/
+  echo "Commiting release"
+  git add -A .
+  git commit -m "Release sources for $os/$variant/head"
+  echo "Creating tag"
+  git tag $os/$variant/head --force
+  echo "Created tag $os/$variant/head on branch release/$os/$variant"
+  echo "Finishing..."
+  git checkout $current_branch
+  echo "DONE!"
+}
 
-echo "Clearing 'releases' folder"
-rm -rf releases/*
-echo "Copying files from $current_branch"
-git checkout $current_branch -- releases/
-echo "Commiting release"
-git add -A .
-git commit -m "Release sources for $os/$variant/head"
-echo "Creating tag"
-git tag $os/$variant/head --force
-echo "Created tag $os/$variant/head on branch release/$os/$variant"
-echo "Finishing..."
-git checkout $current_branch
-echo "DONE!"
+tix_release_version() {
+  create_new_tag
+}
 
+tix_release_version "$@"
