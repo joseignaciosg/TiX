04e5d90137a231610eede97626c24ca83fba30a5
Cristian Pereyra <cristian@redmintlabs.com>
2014-10-04 13:00:45 -0300

    x64 build

--- releases/TixApp/InstallerFiles/startupAppCaller.py
+++
@@ -1,9 +0,0 @@
-import shutil,errno,os,stat,platform,sys,getopt
-
-installDirUnix = "/etc/TIX"
-installDirUnixApp = installDirUnix + '/app'
-udpclientFile = "TixClientApp.py"
-installDirUnixAppExecutable = installDirUnixApp + '/' + udpclientFile
-
-if __name__ == "__main__":
-	os.system("sudo " + installDirUnixAppExecutable + " log")
